689cc0b16cb82651d3138c0ad46caba8fc693abb
Gede Rajeg <primahadiwijaya@gmail.com>
2025-04-05 09:28:15 +0800

    Add the orthography profiling init code.

---
+++ etc/02-orthography-cldf.sh
@@ -0,0 +1,87 @@
+lingpy profile --clts --cldf --column=form --context -i cldf/cldf-metadata.json -o etc/orthography.tsv
+#  cldf/cldf-metadata.json -o etc/orthography.tsv
+# 2025-04-05 09:19:25,871 [INFO] Successfully changed parameters.
+# 2025-04-05 09:19:25,871 [INFO] Successfully changed parameters.
+# iter words:   0%|                                                            | 0/80 [00:00<?, ?it/s]2025-04-05 09:19:26,116 [INFO] processing 1-satoe
+# 2025-04-05 09:19:26,116 [INFO] processing 2-sije
+# 2025-04-05 09:19:26,116 [INFO] processing 3-do
+# 2025-04-05 09:19:26,116 [INFO] processing 4-sada
+# 2025-04-05 09:19:26,117 [INFO] processing 5-sa
+# 2025-04-05 09:19:26,117 [INFO] processing 6-sara
+# 2025-04-05 09:19:26,117 [INFO] processing 7-sara
+# 2025-04-05 09:19:26,117 [INFO] processing 8-daheij
+# 2025-04-05 09:19:26,117 [INFO] processing 9-doea
+# 2025-04-05 09:19:26,117 [INFO] processing 10-rowa
+# 2025-04-05 09:19:26,117 [INFO] processing 11-dooij
+# 2025-04-05 09:19:26,117 [INFO] processing 12-doeo
+# 2025-04-05 09:19:26,117 [INFO] processing 13-doea
+# 2025-04-05 09:19:26,117 [INFO] processing 14-doea
+# 2025-04-05 09:19:26,117 [INFO] processing 15-doea
+# 2025-04-05 09:19:26,117 [INFO] processing 16-adoea
+# 2025-04-05 09:19:26,117 [INFO] processing 17-tiga
+# 2025-04-05 09:19:26,117 [INFO] processing 18-pullo
+# 2025-04-05 09:19:26,117 [INFO] processing 19-tellau
+# 2025-04-05 09:19:26,117 [INFO] processing 20-tolo
+# 2025-04-05 09:19:26,117 [INFO] processing 21-tlo
+# 2025-04-05 09:19:26,117 [INFO] processing 22-feloe
+# 2025-04-05 09:19:26,117 [INFO] processing 23-teloe
+# 2025-04-05 09:19:26,117 [INFO] processing 24-agoloe
+# 2025-04-05 09:19:26,117 [INFO] processing 25-ampat
+# 2025-04-05 09:19:26,117 [INFO] processing 26-ampa
+# 2025-04-05 09:19:26,117 [INFO] processing 27-mpat
+# 2025-04-05 09:19:26,117 [INFO] processing 28-opat
+# 2025-04-05 09:19:26,117 [INFO] processing 29-pat
+# 2025-04-05 09:19:26,117 [INFO] processing 30-effa
+# 2025-04-05 09:19:26,117 [INFO] processing 31-eppat
+# 2025-04-05 09:19:26,117 [INFO] processing 32-aöpa
+# 2025-04-05 09:19:26,118 [INFO] processing 33-lima
+# 2025-04-05 09:19:26,118 [INFO] processing 34-lema
+# 2025-04-05 09:19:26,118 [INFO] processing 35-lema
+# 2025-04-05 09:19:26,118 [INFO] processing 36-lema
+# 2025-04-05 09:19:26,118 [INFO] processing 37-lemoeng
+# 2025-04-05 09:19:26,118 [INFO] processing 38-limang
+# 2025-04-05 09:19:26,118 [INFO] processing 39-liman
+# 2025-04-05 09:19:26,118 [INFO] processing 40-alima
+# 2025-04-05 09:19:26,118 [INFO] processing 41-anam
+# 2025-04-05 09:19:26,118 [INFO] processing 42-anam
+# 2025-04-05 09:19:26,118 [INFO] processing 43-noom
+# 2025-04-05 09:19:26,118 [INFO] processing 44-onam
+# 2025-04-05 09:19:26,118 [INFO] processing 45-nam
+# 2025-04-05 09:19:26,118 [INFO] processing 46-e nem
+# 2025-04-05 09:19:26,118 [INFO] processing 47-ennem
+# 2025-04-05 09:19:26,118 [INFO] processing 48-akiakoio
+# 2025-04-05 09:19:26,118 [INFO] processing 49-toedjoe
+# 2025-04-05 09:19:26,118 [INFO] processing 50-peto
+# 2025-04-05 09:19:26,118 [INFO] processing 51-tojoa
+# 2025-04-05 09:19:26,118 [INFO] processing 52-paito
+# 2025-04-05 09:19:26,118 [INFO] processing 53-tojo
+# 2025-04-05 09:19:26,118 [INFO] processing 54-fitoe
+# 2025-04-05 09:19:26,118 [INFO] processing 55-pitoe
+# 2025-04-05 09:19:26,118 [INFO] processing 56-alima-adoea
+# 2025-04-05 09:19:26,118 [INFO] processing 57-delapan
+# 2025-04-05 09:19:26,118 [INFO] processing 58-oallo
+# 2025-04-05 09:19:26,118 [INFO] processing 59-delapon
+# 2025-04-05 09:19:26,118 [INFO] processing 60-oallo
+# 2025-04-05 09:19:26,118 [INFO] processing 61-dlappang
+# 2025-04-05 09:19:26,118 [INFO] processing 62-walloe
+# 2025-04-05 09:19:26,118 [INFO] processing 63-walloe
+# 2025-04-05 09:19:26,118 [INFO] processing 64-alima-agoloe
+# 2025-04-05 09:19:26,119 [INFO] processing 65-sembilan
+# 2025-04-05 09:19:26,119 [INFO] processing 66-sewa
+# 2025-04-05 09:19:26,119 [INFO] processing 67-sempilan
+# 2025-04-05 09:19:26,119 [INFO] processing 68-sea
+# 2025-04-05 09:19:26,119 [INFO] processing 69-sakorang
+# 2025-04-05 09:19:26,119 [INFO] processing 70-siwa
+# 2025-04-05 09:19:26,119 [INFO] processing 71-siwa
+# 2025-04-05 09:19:26,119 [INFO] processing 72-alima-aöpa
+# 2025-04-05 09:19:26,119 [INFO] processing 73-sapoeloe
+# 2025-04-05 09:19:26,119 [INFO] processing 74-polo
+# 2025-04-05 09:19:26,119 [INFO] processing 75-depoolo
+# 2025-04-05 09:19:26,119 [INFO] processing 76-sapolo
+# 2025-04-05 09:19:26,119 [INFO] processing 77-sapolo
+# 2025-04-05 09:19:26,119 [INFO] processing 78-foeloe
+# 2025-04-05 09:19:26,119 [INFO] processing 79-poeloe
+# 2025-04-05 09:19:26,119 [INFO] processing 80-tahapoeloe
+# 2025-04-05 09:19:26,121 [INFO] Data has been written to file <etc/orthography.tsv>.
+
+# git add etc/orthography.tsv <- to track the editing
