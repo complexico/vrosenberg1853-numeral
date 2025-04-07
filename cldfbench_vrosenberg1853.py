b405588c6ecfd60ce88738c6188aedb1a8a5c658
Enggano <engganolang@gmail.com>
2025-04-07 10:46:47 +0800

    Minor update to codes.

--- cldfbench_vrosenberg1853.py
+++ cldfbench_vrosenberg1853.py
@@ -40,47 +40,49 @@ class Dataset(BaseDataset):
         pass
 
     def cmd_makecldf(self, args):
+
+        # personal note: this `add_sources()` will presumably output the "sources.bib" inside cldf because the "sources.bib" under raw is self-added before cldf conversion
         args.writer.add_sources()
         args.log.info("added sources")
 
-        # add languages
+        # add languages (personal note: this `add_languages` function will output the "languages.csv" inside cldf)
         languages = args.writer.add_languages(
-            id_factory=lambda l: l["Name"], lookup_factory=lambda l: (l["Name"], l["Sources"])
+            id_factory=lambda l: l["Name"], lookup_factory = lambda l: (l["Name"], l["Sources"])
         )
         sources = {k[0]: k[1] for k in languages}
         # IMPORTANT: do not put space in the values of the language "Name" column
 
         # def cln(word): return re.sub("[†\\d\\.\\*\\?\\~]", "", word)
 
-        # add concept
+        # add concept (personal note: this `add_concept()` call will output the "parameters.csv" inside cldf which is taken from etc/concepts.tsv)
         concepts = {}
         for i, concept in enumerate(self.concepts):
             idx = str(i + 1) + "_" + slug(concept["Gloss"])
             args.writer.add_concept(
-                ID=idx,
-                Name=concept["Gloss"],
-                Number=concept["Number"],
-                Concepticon_ID=concept["Concepticon_ID"],
-                Concepticon_Gloss=concept["Concepticon_Gloss"]
+                ID = idx,
+                Name = concept["Gloss"],
+                Number = concept["Number"],
+                Concepticon_ID = concept["Concepticon_ID"],
+                Concepticon_Gloss = concept["Concepticon_Gloss"]
             )
             concepts[concept["Gloss"], concept["Concepticon_ID"]] = idx
             #concepts[concept["Concepticon_ID"]] = idx
         args.log.info("added concepts")
 
         for idx, row in enumerate(self.raw_dir.read_csv(
-            "raw-dat.tsv", delimiter="\t", dicts=True)):
-            #args.writer.add_form_with_segments( # this add_form_with_segments() is used when to include the Segments info from the raw data (this function also requires explicit statement of the addition of the Form variable). But, args.writer.add_forms_from_value is used when we can ignore the Segments variable
+            "raw-dat.tsv", delimiter = "\t", dicts = True)):
+            # args.writer.add_form_with_segments( # this add_form_with_segments() is used when to include the Segments info from the raw data (this function also requires explicit statement of the addition of the Form variable). But, args.writer.add_forms_from_value is used when we can ignore the Segments variable
             args.writer.add_forms_from_value(
-                Local_ID=row["ID"],
-                Language_ID=row["Doculect"],
+                Local_ID = row["ID"],
+                Language_ID = row["Doculect"],
                 # For the following line of code in getting the Parameter_ID to work, we need to:
                 # - ensure that the CONCEPTICON_ID in the raw/main data and in the Concepts.tsv data is the same.
-                #   That is, we cannot have CONCEPTICON_ID of NA (in raw/main data) but empty in Concepts.tsv data.
-                #   If that is the case, it will throw KeyError such as `KeyError: ('this and that', 'NA')` (in this case, the function found this key: 'this and that', 'NA' in the raw/main data but in the concepts dictionary, it is 'this and that', '', which is different!)
-                Parameter_ID=concepts[row["English"], row["Concepticon_ID"]], 
-                # CommonTranscription=row["CommonsNotSegmented"], # this column takes the non-tokenised common transcription
-                Value=row["Form"], # this column takes the original transcription
-                #Value=row["Mentawai"], # specify this line for the Value col. explicitly when args.writer.add_form_with_segments() is used
-                #Form=row["CommonsNotSegmented"], # specify this line for the Segments col. explicitly when args.writer.add_form_with_segments() is used
-                #Segments=list(row["IPA"]), # specify this line for the Segments col. explicitly when args.writer.add_form_with_segments() is used; the Segments column needs a list (not string), that is why we use the list() function
-                Source="VonRosenberg1853")
+                #   That is, we cannot have a CONCEPTICON_ID of NA (in the main data [whatever we call this main data] under raw) but then empty/NULL in Concepts.tsv data.
+                #   If that is the case, it will throw a KeyError such as `KeyError: ('this and that', 'NA')` (in this case, the function found this key: 'this and that', 'NA' in the raw/main data but in the concepts dictionary, it is 'this and that', '', which is different!)
+                Parameter_ID = concepts[row["English"], row["Concepticon_ID"]], 
+                # CommonTranscription = row["CommonsNotSegmented"], # this column takes the non-tokenised common transcription
+                Value = row["Form"], # this column takes the original transcription
+                # Value = row["Mentawai"], # specify this line for the Value col. explicitly when args.writer.add_form_with_segments() is used
+                # Form = row["CommonsNotSegmented"], # specify this line for the Segments col. explicitly when args.writer.add_form_with_segments() is used
+                #Segments = list(row["IPA"]), # specify this line for the Segments col. explicitly when args.writer.add_form_with_segments() is used; the Segments column needs a list (not string), that is why we use the list() function
+                Source = "VonRosenberg1853")
--- etc/cognacy-data-combine-with-main.r
+++ etc/cognacy-data-combine-with-main.r
@@ -1,10 +1,29 @@
 library(tidyverse)
 
+# This code is to be run when the following conditions are met:
+##
+## 1. an existing cldf dataset (with FormTable that contains segmented tokens). This condition also entails:
+##    
+##    1.1 an initial orthography profile overall and/or per language existed (preferrably also edited and checked)
+## 
+## 2. an input data containing the segmented tokens to be processed with lingpy's LexStat and Alignment to provide initial cognate identification and alignment between tokens/segments
+##
+## 3. an output file as the results for running initial cognate identification and alignment analysis. 
+##
+##    3.1 This output cognate identification file is to be the basis for joining with the initial raw data
+##    3.2 This joined RAW DATA and COGNATE+ALIGNMENT OUTPUT will be processed again into CLDF dataset (step 1 above)
+
 cogid <- read_tsv("etc/cognacy-vrosenberg1853-output.tsv")
 
 rawdat <- read_tsv("raw/raw-dat.tsv")
 
 cogid_mini <- cogid |>
-    select(ID, Doculect = DOCULECT, COGID, ALIGNMENT)
+    select(ID, English = CONCEPT, 
+            Doculect = DOCULECT, 
+            COGNACY = COGID, ALIGNMENT)
 
-rawdat |> left_join(cogid_mini)
+rawdat_with_cognate <- rawdat |> 
+    left_join(cogid_mini)
+
+colnames(rawdat)
+colnames(rawdat_with_cognate)
